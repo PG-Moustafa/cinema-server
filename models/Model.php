29926b978b46122e170f52e23ab4033a583a5639
MOUSTAFA HAYDAR <programmermoustafa@gmail.com>
2025-06-27 02:08:20 +0300

    Add Model model & Movie model

---
+++ models/Model.php
@@ -0,0 +1,119 @@
+<?php
+abstract class Model
+{
+    protected static string $table;
+    protected static string $primary_key = "id";
+
+    public static function find(mysqli $mysqli, int $id)
+    {
+        $sql = sprintf(
+            "Select * from %s WHERE %s = ?",
+            static::$table,
+            static::$primary_key
+        );
+
+        $query = $mysqli->prepare($sql);
+        $query->bind_param("i", $id);
+        $query->execute();
+
+        $data = $query->get_result()->fetch_assoc();
+
+        return $data ? new static($data) : null;
+    }
+
+    public static function all(mysqli $mysqli)
+    {
+        $sql = sprintf(
+            "SELECT * FROM %s",
+            static::$table
+        );
+
+        $query = $mysqli->prepare($sql);
+        $query->execute();
+        $data = $query->get_result();
+
+        $objects = [];
+        while ($row = $data->fetch_assoc()) {
+            $objects[] = new static($row);
+        }
+        return $objects;
+    }
+
+    public static function create(mysqli $mysqli, array $data)
+    {
+        $columns = array_keys($data);
+        $placeholder = implode(", ", array_fill(0, count($columns), "?"));
+        $columns_list = implode(", ", $columns);
+
+        $sql = sprintf(
+            "INSERT INTO %s ($columns_list) VALUES ($placeholder)",
+            static::$table
+        );
+        $query = $mysqli->prepare($sql);
+
+        $types = "";
+        $values = [];
+
+        foreach ($data as $value) {
+            $types .= match (true) {
+                is_int($value) => 'i',
+                is_float($value) => 'd',
+                default => 's',
+            };
+            $values[] = $value;
+        }
+
+        $query->bind_param($types, ...$values);
+
+        return $query->execute();
+
+    }
+
+    public function update(mysqli $mysqli, array $data): bool
+    {
+        $columns = array_keys($data);
+        $assignments = implode(", ", array_map(fn($col) => "$col = ?", $columns));
+
+        $sql = sprintf(
+            "UPDATE %s SET %s WHERE %s = ?",
+            static::$table,
+            $assignments,
+            static::$primary_key
+        ); 
+
+        $query = $mysqli->prepare($sql);
+
+        $types = "";
+        $values = [];
+
+        foreach ($data as $value) {
+            $types .= match (true) {
+                is_int($value) => 'i',
+                is_float($value) => 'd',
+                default => 's',
+            };
+            $values[] = $value;
+        }
+
+        $types .= 'i';
+        $values[] = $this->{static::$primary_key};
+
+        $query->bind_param($types, ...$values);
+        return $query->execute();
+    }
+
+
+
+}
+
+
+
+
+//you have to continue with the same mindset
+//Find a solution for sending the $mysqli everytime...
+//Implement the following:
+//1- update() -> non-static function - completing
+//2- create() -> static function - done
+//3- delete() -> non-static function
+
+?>
---
+++ models/Movie.php
@@ -0,0 +1,111 @@
+<?php
+require_once("Model.php");
+
+class Movie extends Model
+{
+    protected ?int $id;
+    private string $title;
+    private string $description;
+    private string $rating;
+    private string $release_date;
+    private int $duration_minutes;
+    private string $poster_url;
+    protected static string $table = "movies";
+
+    public function __construct(array $data)
+    {
+        $this->id = $data["id"] ?? null;
+        $this->title = $data["title"];
+        $this->description = $data["description"];
+        $this->rating = $data["rating"];
+        $this->release_date = $data["release_date"];
+        $this->duration_minutes = $data["duration_minutes"];
+        $this->poster_url = $data["poster_url"];
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getRating(): string
+    {
+        return $this->rating;
+    }
+
+    public function getRelease_date(): string
+    {
+        return $this->release_date;
+    }
+
+    public function getDuration_Minutes(): int
+    {
+        return $this->duration_minutes;
+    }
+
+    public function getPoster_url(): string
+    {
+        return $this->poster_url;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function setRating(string $rating): void
+    {
+        $this->rating = $rating;
+    }
+
+    public function setRelease_Date(string $release_date): void
+    {
+        $this->release_date = $release_date;
+    }
+
+    public function setDuration_Minutes(int $duration_minutes): void
+    {
+        $this->duration_minutes = $duration_minutes;
+    }
+
+    public function setPoster_Url(string $poster_url): void
+    {
+        $this->poster_url = $poster_url;
+    }
+
+    public function toArray()
+    {
+        $data = [
+            "title" => $this->title,
+            "description" => $this->description,
+            "rating" => $this->rating,
+            "release_date" => $this->release_date,
+            "duration_minutes" => $this->duration_minutes,
+            "poster_url" => $this->poster_url
+        ];
+
+        if (isset($this->id)) {
+            $data["id"] = $this->id;
+        }
+
+        return $data;
+    }
+
+}
+
+?>
